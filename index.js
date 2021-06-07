80977180ee80baae6a5ce411154257ab585f4990
Trace <tracedelange@me.com>
2021-06-07 13:51:24 -0500

    commit tree df96e9a57c53864dc924bfbe8c8ddb3c3eead2cd

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(42 - street)
+};
+
+function distanceFromHqInFeet(street) {
+    return (distanceFromHqInBlocks(street))*264
+};
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(end-start)*264
+};
+
+function calculatesFarePrice(start, end) {
+    const billableDistance = (distanceTravelledInFeet(start, end))-400
+
+    if (billableDistance < 0) {
+        return 0
+    } else if (billableDistance <= 1600) {
+        return billableDistance*0.02
+    } else if (billableDistance > 1600 && billableDistance < 2100) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+    
+
+}
